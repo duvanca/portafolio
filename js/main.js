64f9adaad4eb2cc0e7a435ef939297dd78a7be2a
duvanca <73137805+duvanca@users.noreply.github.com>
2022-08-13 16:50:35 -0500

    local storage

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
 const btnswitch = document.querySelector("#switch");
 
-btnswitch.addEventListener("click", ()=>{
+btnswitch.addEventListener("click", () => {
     document.body.classList.toggle("dark");
     btnswitch.classList.toggle("active");
+
+    if (document.body.classList.contains("dark")) {
+        localStorage.setItem("dark-mode", "true");
+    } else {
+        localStorage.setItem("dark-mode", "false")
+    }
 })
+
+
+if (localStorage.getItem("dark-mode") === "true") {
+    document.body.classList.add("dark");
+    btnswitch.classList.add("active");
+} else {
+    document.body.classList.remove("dark");
+    btnswitch.classList.remove("active");
+}
